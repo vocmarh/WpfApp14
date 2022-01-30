178647776d6e369e4d87b980f83dd1c680e2a80b
Sergei Khramtsov <vocmarh1989@gmail.com>
2022-01-30 23:02:14 +0500

    Add project files.

---
+++ WpfApp14/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> products;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                NameProduct = "Помидоро",
+                Price = 126,
+                Image = "Pictures/Pomidoro.jpg",
+                Category = Category.Food
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Банан",
+                Price = 90,
+                Image = "Pictures/Banana.jpg",
+                Category = Category.Food
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Купальник",
+                Price = 8000,
+                Image = "Pictures/Swimsuit.jpg",
+                Category = Category.Clothes
+            });
+            products.Add(new Product()
+            {
+                NameProduct = "Кока-кола",
+                Price = 35,
+                Image = "Pictures/Coca-cola.jpg",
+                Category = Category.Beverages
+            });
+            IstBox.ItemsSource = products;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            products.Add(new Product()
+            {
+                NameProduct = "Стриральная машина",
+                Price = 32000,
+                Image = "Pictures/Washing machine.jpg",
+                Category = Category.Technics
+            });
+        }
+    }
+}
---
+++ WpfApp14/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp14
+{
+    public enum Category
+    {
+        Food,
+        Beverages,
+        Clothes,
+        Technics
+    }
+    internal class Product
+    {
+        public string NameProduct { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public Category Category { get; set; }
+        public override string ToString()
+        {
+            return NameProduct;
+        }
+    }
+}
+
